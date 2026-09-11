106e77d7483c1d0fb072ba65054cba1043101bff
ShobujDash <shobujd322@gmail.com>
2026-09-11 16:44:53 +0600

    first hello world [Done]

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Learn',
+      home: Container(
+        color: Colors.indigo,
+        child: const Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
