6ca59a342a02315ddf91e6720484c397d7d1effa
ShobujDash <shobujd322@gmail.com>
2026-09-11 16:53:00 +0600

    Home view Initialization [Done]

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/home_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Learn',
-      home: Container(
-        color: Colors.indigo,
-        child: const Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Flutter Learn', home: const HomeView());
   }
 }
